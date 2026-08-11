c0b6ba1dfb726ef710447f80c10bda6b7c989622
Prathamesh <prash6130@gmail.com>
2026-08-11 17:36:38 +0530

    folders added

---
+++ backend/db/db.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+const dns=require('dns')
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+])
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+module.exports=connectDB;
---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const app=require('./src/app')
+const connectDB=require('./db/db')
+require('dotenv').config();
+const PORT=process.env.PORT || 5000
+connectDB();
+app.listen(PORT, ()=>[
+    console.log(`Server is running on port ${PORT}`)
+])
